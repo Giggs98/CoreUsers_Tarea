7606695049dc04c8824fd9560f9bfafbb1eb811f
Gibran <84532008+Gibran9807@users.noreply.github.com>
2021-10-21 14:55:01 -0500

    Tarea

---
+++ Users_CORE/Configuration/ConnectionStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Users_CORE.Configuration
+{
+    public static class ConnectionStrings
+    {
+        public static string LocalServer = "ZKvO+FHjoZKEEE76jdbyAsyu4Su4tkIwxW8byPwb4BlMCTbZjlDSuE1jhSlxaXvTJY/GOscdAYgFaCpdTByuxMlDzlVR7IBRbHbhGF7A4PBjWCuKz6QCC8L6cp7prLVEmNVcuhTyhifC/5y/qRvywg2U3sKtKYV8Y2x1WWtLyxh6QKIT0jNRN1KOl0GHLWPsN1ScLvHnZ8OGoJjB/IIDKg==";
+
+        public static string CloudServer = "";
+
+        public static string SecretKey = "MtWDMC0v1d2021";
+    }
+}
